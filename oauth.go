e414d7969dabbce741c4f9ddda6c6e7e3c56cb50
Paul Stanley <paulstanley@pendo.io>
2021-05-21 17:43:09 -0400

    Minimal changes to support oauth v2

--- oauth.go
+++ oauth.go
@@ -29,6 +29,40 @@ type OAuthResponse struct {
 	SlackResponse
 }
 
+// OAuthV2Response ...
+type OAuthV2Response struct {
+	AccessToken     string                       `json:"access_token"`
+	TokenType       string                       `json:"token_type"`
+	Scope           string                       `json:"scope"`
+	BotUserID       string                       `json:"bot_user_id"`
+	AppID           string                       `json:"app_id"`
+	Team            OAuthV2ResponseTeam          `json:"team"`
+	IncomingWebhook OAuthResponseIncomingWebhook `json:"incoming_webhook"`
+	Enterprise      OAuthV2ResponseEnterprise    `json:"enterprise"`
+	AuthedUser      OAuthV2ResponseAuthedUser    `json:"authed_user"`
+	SlackResponse
+}
+
+// OAuthV2ResponseTeam ...
+type OAuthV2ResponseTeam struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+// OAuthV2ResponseEnterprise ...
+type OAuthV2ResponseEnterprise struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+// OAuthV2ResponseAuthedUser ...
+type OAuthV2ResponseAuthedUser struct {
+	ID          string `json:"id"`
+	Scope       string `json:"scope"`
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+}
+
 // GetOAuthToken retrieves an AccessToken
 func GetOAuthToken(clientID, clientSecret, code, redirectURI string, debug bool) (accessToken string, scope string, err error) {
 	return GetOAuthTokenContext(context.Background(), clientID, clientSecret, code, redirectURI, debug)
@@ -63,4 +97,24 @@ func GetOAuthResponseContext(ctx context.Context, clientID, clientSecret, code, 
 		return nil, errors.New(response.Error)
 	}
 	return response, nil
+}
+
+// GetOAuthV2Response gets a V2 OAuth access token response - https://api.slack.com/methods/oauth.v2.access
+func GetOAuthV2Response(client HTTPRequester, clientID, clientSecret, code, redirectURI string) (resp *OAuthV2Response, err error) {
+	return GetOAuthV2ResponseContext(context.Background(), client, clientID, clientSecret, code, redirectURI)
+}
+
+// GetOAuthV2ResponseContext with a context, gets a V2 OAuth access token response
+func GetOAuthV2ResponseContext(ctx context.Context, client HTTPRequester, clientID, clientSecret, code, redirectURI string) (resp *OAuthV2Response, err error) {
+	values := url.Values{
+		"client_id":     {clientID},
+		"client_secret": {clientSecret},
+		"code":          {code},
+		"redirect_uri":  {redirectURI},
+	}
+	response := &OAuthV2Response{}
+	if err = postForm(ctx, client, SLACK_API+"oauth.v2.access", values, response, false); err != nil {
+		return nil, err
+	}
+	return response, response.Err()
 }
